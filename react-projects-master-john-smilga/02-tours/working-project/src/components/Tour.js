ed9e50f2ced0452a234f60387bc080176d1556d4
Panji <panji9502@gmail.com>
2023-03-02 16:04:37 +0700

    first commit

---
+++ react-projects-master-john-smilga/02-tours/working-project/src/components/Tour.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const Tour = ({ tour, removeTour }) => {
+  const { id, image, info, name, price } = tour;
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            onClick={() => {
+              setReadMore(!readMore);
+            }}
+          >
+            {readMore ? "show less" : "show more"}
+          </button>
+        </p>
+        <button onClick={() => removeTour(id)} className="delete-btn">
+          not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
