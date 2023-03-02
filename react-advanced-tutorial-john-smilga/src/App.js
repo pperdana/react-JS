e407cee89dd41fc90f57d48b25293b22abcf7328
Panji <panji9502@gmail.com>
2023-03-02 16:36:05 +0700

    add tutorial directory

---
+++ react-advanced-tutorial-john-smilga/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Final from "./tutorial/6-useReducer/final";
+import Setup from "./tutorial/10-prop-types/setup";
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
