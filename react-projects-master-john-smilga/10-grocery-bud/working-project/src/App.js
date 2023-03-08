359b8448cc7f40c245242388cac7b63e968bea3f
Panji <panji9502@gmail.com>
2023-03-08 21:25:09 +0700

    Grocery Bud - Add Items

--- react-projects-master-john-smilga/10-grocery-bud/working-project/src/App.js
+++ react-projects-master-john-smilga/10-grocery-bud/working-project/src/App.js
@@ -4,14 +4,23 @@ import Alert from "./Alert";
 
 function App() {
   const [name, setName] = useState("");
-  const [list, letList] = useState([]);
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello");
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
   return (
     <section className="section-center">
@@ -31,10 +40,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List />
-        <button className="clear-btn">clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
