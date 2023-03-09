fd1dafdfe6e9e7f7d9e7407469af09bdf92b08f2
Panji <panji9502@gmail.com>
2023-03-09 07:43:30 +0700

    Navbar - Data

--- react-projects-master-john-smilga/11-navbar/working-project/src/Navbar.js
+++ react-projects-master-john-smilga/11-navbar/working-project/src/Navbar.js
@@ -13,38 +13,27 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        <div className="link-container show-container">
+        <div className="links-container show-container">
           <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <ul className="social-icons">
-          <li>
-            <a href="http://google.com/">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="http://google.com/">
-              <FaTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="http://google.com/">
-              <FaTwitter />
-            </a>
-          </li>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
