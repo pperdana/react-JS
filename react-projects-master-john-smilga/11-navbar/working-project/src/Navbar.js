2d2c036f2df4f69ef11a0100363463257d44b10f
Panji <panji9502@gmail.com>
2023-03-09 09:05:43 +0700

    Navbar - useRef

--- react-projects-master-john-smilga/11-navbar/working-project/src/Navbar.js
+++ react-projects-master-john-smilga/11-navbar/working-project/src/Navbar.js
@@ -5,11 +5,23 @@ import logo from "./logo.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="logo" />
+          <img src={logo} className="logo" alt="logo" />
           <button
             className="nav-toggle"
             onClick={() => setShowLinks(!showLinks)}
@@ -18,12 +30,8 @@ const Navbar = () => {
           </button>
         </div>
 
-        <div
-          className={`${
-            showLinks ? "links-container show-container" : "links-container"
-          }`}
-        >
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
