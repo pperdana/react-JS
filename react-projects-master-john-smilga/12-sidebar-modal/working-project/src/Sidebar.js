8d961a4e815e513efb8dea766fea198904e01148
Panji <panji9502@gmail.com>
2023-03-09 21:05:32 +0700

    Sidebar/Modal - Complete

--- react-projects-master-john-smilga/12-sidebar-modal/working-project/src/Sidebar.js
+++ react-projects-master-john-smilga/12-sidebar-modal/working-project/src/Sidebar.js
@@ -2,13 +2,16 @@ import React from "react";
 import logo from "./logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
+  const { isSideBarOpen, closeSideBar } = useGlobalContext();
+
   return (
-    <aside className={`sidebar`}>
+    <aside className={`${isSideBarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} alt="coding" className="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSideBar}>
           <FaTimes />
         </button>
       </div>
--- react-projects-master-john-smilga/12-sidebar-modal/working-project/src/context.js
+++ react-projects-master-john-smilga/12-sidebar-modal/working-project/src/context.js
@@ -3,7 +3,37 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSideBarOpen,
+        openModal,
+        openSideBar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 // custom hook
 export const useGlobalContext = () => {
