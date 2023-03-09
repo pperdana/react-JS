763b095645767542d796bb9e644ef0af420e2ee8
Panji <panji9502@gmail.com>
2023-03-10 06:40:21 +0700

    Stripe - Context Boilerplate

---
+++ react-projects-master-john-smilga/13-stripe-submenus/working-project/src/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
+
+const Hero = () => {
+  return <h2>hero component</h2>;
+};
+
+export default Hero;
---
+++ react-projects-master-john-smilga/13-stripe-submenus/working-project/src/context.js
@@ -0,0 +1,43 @@
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubMenu = () => {
+    setIsSubMenuOpen(true);
+  };
+
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSubMenuOpen,
+        isSidebarOpen,
+        openSubMenu,
+        openSidebar,
+        closeSubMenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
