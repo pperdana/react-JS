b96c4aa9af2904e807753967e935a25d8f007143
Panji <panji9502@gmail.com>
2023-03-10 07:00:08 +0700

    Stripe - Hero

--- react-projects-master-john-smilga/13-stripe-submenus/working-project/src/Hero.js
+++ react-projects-master-john-smilga/13-stripe-submenus/working-project/src/Hero.js
@@ -3,7 +3,25 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>;
+  const { closeSubMenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <arcticle className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </arcticle>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
