833f97fed4c4cbe876aca2bf0b29a918825a1e83
Panji <panji9502@gmail.com>
2023-05-16 12:01:30 +0700

    useffect code

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,4 +1,5 @@
-import Starter from "./tutorial/01-useState/starter/02-useState-basics";
+import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+
 function App() {
   return (
     <div className="container">
