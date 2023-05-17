2f155ca2163d295897132806326b0a4991a791c5
Panji <panji9502@gmail.com>
2023-05-18 02:28:53 +0700

    add menu code

--- 04-fundamental-projects/00-on-work/src/App.jsx
+++ 04-fundamental-projects/00-on-work/src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from "react";
 
-import data from "./data";
-import Questions from "./Questions";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
-  const [questions, setQuestions] = useState(data);
-
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
-      <Questions questions={questions} />
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
     </main>
   );
 };
