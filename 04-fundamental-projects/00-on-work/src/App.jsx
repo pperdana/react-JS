69fb80113a9ab9fc10b18471ad9768d75aed7698
Panji <panji9502@gmail.com>
2023-05-23 02:11:53 +0700

    update with styled component | birthday buddy

--- 04-fundamental-projects/00-on-work/src/App.jsx
+++ 04-fundamental-projects/00-on-work/src/App.jsx
@@ -1,6 +1,7 @@
-import data from "./data";
-import "./App.scss";
 import { useState } from "react";
+import styled from "styled-components";
+
+import data from "./data";
 
 import PeopleList from "./PeopleList";
 
@@ -8,15 +9,63 @@ const App = () => {
   const [people, setPeople] = useState(data);
 
   return (
-    <main>
-      <section className="container">
+    <Main>
+      <Container>
         <h1 className="title">{people.length} birthdays today</h1>
         <PeopleList people={people} />
-        <button className="btn" onClick={() => setPeople([])}>
+        <Button className="btn" onClick={() => setPeople([])}>
           clear all
-        </button>
-      </section>
-    </main>
+        </Button>
+      </Container>
+    </Main>
   );
 };
+
 export default App;
+
+const Main = styled.main`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.section`
+  background-color: #ffffff;
+  width: 60rem;
+  padding: 2.4rem 3.2rem;
+
+  background: #ffffff;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06);
+  border-radius: 4px;
+
+  .title {
+    font-weight: 400;
+    margin-bottom: 3.2rem;
+    text-transform: capitalize;
+    letter-spacing: 1px;
+  }
+
+  .btn:hover {
+    background-color: #a02bb2;
+  }
+`;
+
+const Button = styled.button`
+  display: inline-block;
+
+  font-family: "Arial";
+  text-transform: capitalize;
+  color: #ffffff;
+  letter-spacing: 1px;
+
+  width: 536px;
+  height: 27px;
+  background-color: #d946ef;
+  border: none;
+  border-radius: 5px;
+
+  cursor: pointer;
+  transition: all 0.3s;
+`;
--- 04-fundamental-projects/00-on-work/src/Person.jsx
+++ 04-fundamental-projects/00-on-work/src/Person.jsx
@@ -1,13 +1,42 @@
+import styled from "styled-components";
+
 const Person = ({ image, name, age }) => {
   return (
-    <div className="person">
+    <PersonContainer>
       <img src={image} alt={name} />
       <div className="info">
         <h3>{name}</h3>
         <span>{age} years</span>
       </div>
-    </div>
+    </PersonContainer>
   );
 };
+
+const PersonContainer = styled.div`
+  display: flex;
+  margin-bottom: 2.4rem;
+
+  img {
+    width: 75px;
+    height: 75px;
+    object-fit: cover;
+    border-radius: 50%;
+  }
+
+  .info {
+    margin-left: 1.2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    h3 {
+      letter-spacing: 1px;
+    }
+
+    span {
+      color: #64748b;
+    }
+  }
+`;
 
 export default Person;
