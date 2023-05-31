2563e64e00f50b28c146963c89205d7a8b5325bb
Panji <panji9502@gmail.com>
2023-05-31 15:31:09 +0700

    add react code | lorem ipsum

--- 04-fundamental-projects/00-on-work/src/App.jsx
+++ 04-fundamental-projects/00-on-work/src/App.jsx
@@ -1,32 +1,58 @@
 import { useState } from "react";
-import data from "./data";
+import text from "./data";
+import { nanoid } from "nanoid";
+
 import "./App.scss";
 
-import Title from "./Title";
-import Menu from "./Menu";
-import Categories from "./Categories";
-
-const categories = ["all", ...new Set(data.map((item) => item.category))];
-
 const App = () => {
-  const [menu, setMenu] = useState(data);
-  const [catList, setCatList] = useState(categories);
+  const [num, setNum] = useState(1);
+  const [content, setContent] = useState([]);
 
-  const filterCat = (category) => {
-    let newMenu = data.filter((item) => item.category === category);
-    if (category === "all") {
-      newMenu = data;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleGenerate = () => {
+    if (num < text.length && num > 0) {
+      const newContent = text.slice(0, num);
+      setContent(newContent);
+      return;
     }
-    setMenu(newMenu);
+    setContent([]);
   };
 
   return (
     <main>
-      <section className="container">
-        <Title />
-        <Categories catList={catList} filterCat={filterCat} />
-        <Menu menu={menu} />
+      <section className="main-container">
+        <h1 className="heading-primary">tired of boring lorem ipsum?</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="number" className="form-label">
+            paragraphs:
+          </label>
+          <input
+            type="number"
+            id="number"
+            value={num}
+            onChange={(e) => setNum(e.target.value)}
+            min={1}
+            max={8}
+            className="form-number"
+          />
+          <button className="btn" onClick={handleGenerate}>
+            generate
+          </button>
+        </form>
       </section>
+
+      <article className="text-container">
+        {content.map((item) => {
+          return (
+            <p key={nanoid()} className="paragraph">
+              {item}
+            </p>
+          );
+        })}
+      </article>
     </main>
   );
 };
--- 04-fundamental-projects/00-on-work/src/Title.jsx
+++
@@ -1,5 +0,0 @@
-const Title = () => {
-  return <h1 className="heading-primary">our menu</h1>;
-};
-
-export default Title;
