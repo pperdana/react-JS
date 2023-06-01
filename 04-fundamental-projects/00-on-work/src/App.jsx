a593227686e777bd7803aa0acf83a581901eec14
Panji <panji9502@gmail.com>
2023-06-01 17:01:47 +0700

    add react code | accordion

--- 04-fundamental-projects/00-on-work/src/App.jsx
+++ 04-fundamental-projects/00-on-work/src/App.jsx
@@ -1,58 +1,18 @@
 import { useState } from "react";
-import text from "./data";
-import { nanoid } from "nanoid";
+import data from "./data";
 
+import QuestionList from "./QuestionList";
 import "./App.scss";
 
 const App = () => {
-  const [num, setNum] = useState(1);
-  const [content, setContent] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const handleGenerate = () => {
-    if (num < text.length && num > 0) {
-      const newContent = text.slice(0, num);
-      setContent(newContent);
-      return;
-    }
-    setContent([]);
-  };
+  const [questions, setQuestions] = useState(data);
 
   return (
     <main>
-      <section className="main-container">
-        <h1 className="heading-primary">tired of boring lorem ipsum?</h1>
-        <form className="form" onSubmit={handleSubmit}>
-          <label htmlFor="number" className="form-label">
-            paragraphs:
-          </label>
-          <input
-            type="number"
-            id="number"
-            value={num}
-            onChange={(e) => setNum(e.target.value)}
-            min={1}
-            max={8}
-            className="form-number"
-          />
-          <button className="btn" onClick={handleGenerate}>
-            generate
-          </button>
-        </form>
+      <section className="questions-box">
+        <h1 className="heading-primary">questions</h1>
+        <QuestionList questions={questions} />
       </section>
-
-      <article className="text-container">
-        {content.map((item) => {
-          return (
-            <p key={nanoid()} className="paragraph">
-              {item}
-            </p>
-          );
-        })}
-      </article>
     </main>
   );
 };
