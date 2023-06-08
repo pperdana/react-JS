16f5ac59d7bcc9a60f386b8ecd1ed1beacac0eed
Panji <panji9502@gmail.com>
2023-06-08 21:43:53 +0700

    axios code

--- 05-axios-tutorial/src/App.js
+++ 05-axios-tutorial/src/App.js
@@ -1,8 +1,11 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup from "./examples/5-custom-instance";
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
--- 05-axios-tutorial/src/examples/5-custom-instance.js
+++ 05-axios-tutorial/src/examples/5-custom-instance.js
@@ -1,16 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import authFetch from "../Axios/custom";
+import axios from "axios";
 
-const randomUserUrl = 'https://randomuser.me/api';
+const randomUserUrl = "https://randomuser.me/api";
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    // console.log('custom axios instance');
+    try {
+      const resp1 = await authFetch("./react-store-products");
+      const resp2 = await axios(randomUserUrl);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
