33dc5664540a7c789ac8d521686cb142ec9534ff
Panji <panji9502@gmail.com>
2023-06-09 20:27:47 +0700

    add code

--- 04-fundamental-projects/00-on-work/src/App.jsx
+++ 04-fundamental-projects/00-on-work/src/App.jsx
@@ -1,6 +1,14 @@
-import Navbar from "./Navbar";
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
 };
 export default App;
---
+++ 04-fundamental-projects/00-on-work/src/Modal.jsx
@@ -0,0 +1,18 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Modal = () => {
+  const { isModelOpen, closeModal } = useGlobalContext();
+  return (
+    <div className={isModelOpen ? "modal-overlay show-modal" : "modal-overlay"}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
