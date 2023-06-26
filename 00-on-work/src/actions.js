f889e6f2e73900e1d7a2133e21fcc3d1d7d39b0e
Panji <panji9502@gmail.com>
2023-06-26 22:27:06 +0700

    update code

--- 00-on-work/src/actions.js
+++
@@ -1,6 +0,0 @@
-export const CLEAR_ITEMS = "CLEAR_ITEMS";
-export const REMOVE = "REMOVE";
-export const INCREASE = "INCREASE";
-export const DECREASE = "DECREASE";
-export const LOADING = "LOADING";
-export const DISPLAY_ITEMS = "DISPLAY_ITEMS";
